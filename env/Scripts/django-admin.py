e77f060ce45f7fa39cead98e32c4347c5e24d358
chriskollias <chriskollias500@gmail.com>
2020-05-05 04:20:26 -0700

    created django project

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\chris\dropbox\education\programming_general\python\django\investment_portfolio\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
